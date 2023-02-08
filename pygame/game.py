4349c21a858534e741627c572941be375738b98a
ThomasMaynard <113529499+ThomasMaynard@users.noreply.github.com>
2023-02-08 10:28:58 -0500

    Add files via upload

--- pygame/game.py
+++ pygame/game.py
@@ -11,4 +11,15 @@ def draw():
 def update ():
     pou.left = pou.left + 2
     if pou.left > WIDTH:
-       pou,right = 0
+       pou.right = 0
+
+def on_mouse_down(pos, button):
+    if button == mouse.LEFT and pou.collidepoint(pos):
+        print("Eek!")
+
+def on_mouse_down(pos):
+    if pou.collidepoint(pos):
+        click.wav.play()
+        pou.image = 'character_click'
+        time.sleep(1)
+        pou.image = 'character'
